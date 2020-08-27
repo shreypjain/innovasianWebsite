29bc4dfc8c47608d16809822be7912d20d76fa22
Shrey Jain <jainshreyp@gmail.com>
2020-08-27 17:04:22 -0400

    added new navbar arghhhh

--- src/src/components/navbar.js
+++ src/src/components/navbar.js
@@ -9,22 +9,43 @@ const navbarStyle = {
     right: "0",
     left: "0",
   };
+  function onClick() {
+    let hamburger = document.getElementById('hamburgerbtn')
+    let mobileMenu =document.getElementById('mobileMenu')
 
+    hamburger.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active')
+    })
+}
 function Navbar() {
     return (
-        <nav style={navbarStyle}>
-            <ul class="flex">
-                <li class="flex-1 mr-2">
-                    <a class="text-center block border rounded py-2 px-4 hover:bg-white text-white" href="#">Active Item</a>
-                </li>
-                <li class="flex-1 mr-2">
-                    <a class="text-center block border rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="#">Nav Item</a>
-                </li>
-                <li class="text-center flex-1">
-                    <a class="block py-2 px-4 text-gray-400" href="#">Disabled Item</a>
-                </li>
-            </ul>
-    </nav>
+        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <img src="src/src/assets/logo.jpg"></img>
+    <span class="font-semibold text-xl tracking-tight">Innovasian</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" onClick={onClick()}>
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Ticketing
+      </a>
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Club
+      </a>
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Contact
+      </a>
+    </div>
+    {/* <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div> */}
+  </div>
+</nav>
     )
 }
 export default Navbar
