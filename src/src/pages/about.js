e435003f2362e365fa935f5c11601662bac2ecf9
Shrey Jain <jainshreyp@gmail.com>
2020-09-03 15:11:44 -0400

    added about team cards

--- src/src/pages/about.js
+++ src/src/pages/about.js
@@ -2,13 +2,21 @@ import React from 'react'
 import NavLink from 'react-router-dom'
 
 const bgStyle={
-    background: "linear-gradient(90.05deg, #FB9600 16.94%, #FFFFFF 88.08%)",
+    background: "linear-gradient(90.05deg, #FB9600 16.94%, #FFFFFF 88.08%)"
 }
 
 function About() {
     return(
-        <div className="mt-14 h-screen w-full bg-gray-500" style={bgStyle}>
-            
+        <div>
+            <div className="mt-24 shadow-lg rounded-b-lg sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl overflow-hidden"> 
+            <div className="md-flex bg-white rounded-lg p-6" style={bgStyle}>
+                <img className="h-32 w-48 px-2" src={require("../assets/shrey.jpg")}/>
+                <div className="w-full pr-5 md:w-full md:pr-5 h-full">
+                    <p className="sm:text-base md:text-md w-full mt-3 text-gray-800 text-sm">
+                    </p>
+                </div>
+        </div>
+        </div>
         </div>
     )
 }
