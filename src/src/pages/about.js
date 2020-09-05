62ec91e81aad68cd4491d7b77d7026c8a003d3f0
Shrey Jain <jainshreyp@gmail.com>
2020-09-05 11:28:34 -0400

    hello

--- src/src/pages/about.js
+++ src/src/pages/about.js
@@ -27,16 +27,16 @@ function About() {
             </div>
             <div className="flex mt-2 shadow-lg rounded-b-lg sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative overflow-hidden">
                 <div className="w-1/4">
-                    <img className="mx-4 h-36 w-48 py-4 rounded-lg" src={require("../assets/shrey.jpg")} alt={require("../assets/shashank.jpg")}/>
+                    <img className="mx-4 h-36 w-48 py-4 rounded-lg sm:" src={require("../assets/shashank.jpg")} alt={require("../assets/shashank.jpg")}/>
                 </div>
                 <div className="w-3/4 bg-white">
-                    <h2 className="text-3xl text-purple-500 fond-bold">Shrey Jain</h2>
+                    <h2 className="text-3xl text-purple-500 fond-bold">Shashank Vemuri</h2>
                     <div className="flex mt-2 text-tiny sm:text-xs">
                         <div className="bg-gray-800 rounded-full">
-                            <a className="text-orange-500 font-bold py-1 px-2">Public Relations</a>
+                            <a className="text-orange-500 font-bold py-1 px-2">President</a>
                         </div>
                     </div>
-                    <p className="text-gray-500 mt-2">I am the shows Public Relations coordinator, I made this website, and I bow down to NAV. I hope you enjoy this dumb ass show because I carried.</p>
+                    <p className="text-gray-500 mt-2">I love this club so so much lets build this awesome shit together.</p>
                 </div>
             </div>
             <div className="flex mt-2 shadow-lg rounded-b-lg sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative overflow-hidden">
