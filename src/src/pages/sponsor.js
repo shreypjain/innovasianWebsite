db124a5fdfe6b6fa5872f3516bb2900a26703c60
Shrey Jain <jainshreyp@gmail.com>
2020-09-05 23:47:59 -0400

    hello inno

--- src/src/pages/sponsor.js
+++ src/src/pages/sponsor.js
@@ -3,21 +3,24 @@ import React from 'react'
 function Sponsor() {
     return(
             <div className="mt-2">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="../assets/shrey.jpg" alt="Sunset in the mountains"/>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
+            <h2 className="mt-6 text-center underline text-4xl leading-9 font-extrabold text-gray-900">
+                Sponsors
+              </h2>
+        <div className="mx-auto rounded-b-lg text-center text-l leading-9 text-gray-900 py-4 sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative overflow-hidden">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src={require("../assets/shrey.jpg")} alt="Sunset in the mountains"/>
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Patidar: Cash and Carry</div>
+            </div>
         </div>
-        </div>
+          </div>
+          {/* <div className="mx-auto shadow-lg rounded-b-lg text-center text-l leading-9 text-gray-900 py-4 sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative overflow-hidden"><p>
+          When ordering or registering on our site, as appropriate, you may be asked
+      to enter your name, email address, credit card information or other
+      details to help you with your experience.
+          </p></div> */}
+
+            </div>
     )
 }
 export default Sponsor
