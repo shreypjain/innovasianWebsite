5f37d85b81d5eadda0f6dbfa1f26a4569b2d4d88
Shrey Jain <jainshreyp@gmail.com>
2020-09-08 07:59:25 -0400

    helooo

--- src/src/pages/sponsor.js
+++ src/src/pages/sponsor.js
@@ -29,6 +29,30 @@ function Sponsor() {
         </div>
           </div>
           <div className="flex mx-auto rounded-b-lg text-center text-l leading-9 text-gray-900 py-4 sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative overflow-hidden">
+              <div className="w-1/3 maxw-sm rounded overflow-hidden shadow-lg">
+                  <img className="w-full" src={require("../assets/shrey.jpg")} alt="Patidar"/>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">
+                        Patidar: Cash and Carry
+                </div>
+            </div>
+              </div>
+              <div className="w-1/3 maxw-sm rounded overflow-hidden shadow-lg">
+                  <img className="w-full" src={require("../assets/shrey.jpg")} alt="Patidar"/>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">
+                        Patidar: Cash and Carry
+                </div>
+            </div>
+              </div>
+              <div className="w-1/3 maxw-sm rounded overflow-hidden shadow-lg">
+                  <img className="w-full" src={require("../assets/shrey.jpg")} alt="Patidar"/>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">
+                        Patidar: Cash and Carry
+                </div>
+            </div>
+              </div>
           </div>
     </div>
     )
