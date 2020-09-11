ddb921f6092ead85aafc110fc0b77c50a24bfe62
Shrey Jain <jainshreyp@gmail.com>
2020-09-10 21:33:22 -0400

    heloooo

--- src/src/components/footer.js
+++ src/src/components/footer.js
@@ -8,14 +8,63 @@ const bgStyle = {
 
 function Footer() {
     return (
-      <footer className="">
-        <div className="w-full sm:mx-auto">
-          <div className="flex w-1/3">
-            <h2 className="text-xl">Social Media Links</h2>
+      <div class="container sticky bg-grey-lighter p-8">
+    <div class="mt-24 sm:flex mb-4">
+  <div class="sm:w-1/4 h-auto">
+      <div class="text-orange mb-2">Orange</div>
+      <ul class="list-reset leading-normal">
+          <li class="hover:text-orange-500 text-grey-darker">One</li>
+          <li class="hover:text-orange-500 text-grey-darker">Two</li>
+          <li class="hover:text-orange-500 text-grey-darker">Three</li>
+          <li class="hover:text-orange-500 text-grey-darker">Four</li>
+          <li class="hover:text-orange-500 text-grey-darker">Five</li>
+          <li class="hover:text-orange-500 text-grey-darker">Six</li>
+          <li class="hover:text-orange-500 text-grey-darker">Seven</li>
+          <li class="hover:text-orange-500 text-grey-darker">Eight</li>
+      </ul>
+  </div>
+  <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+      <div class="text-blue mb-2">Blue</div>
+      <ul class="list-reset leading-normal">
+          <li class="hover:text-blue-500 text-grey-darker">One</li>
+          <li class="hover:text-blue-500 text-grey-darker">Two</li>
+          <li class="hover:text-blue-500 text-grey-darker">Three</li>
+      </ul>
 
-          </div>
+    <div class="text-blue-500 mb-2 mt-4">Blue-light</div>
+      <ul class="list-reset leading-normal">
+          <li class="hover:text-blue-500 text-grey-darker">One</li>
+          <li class="hover:text-blue-500 text-grey-darker">Two</li>
+          <li class="hover:text-blue-500 text-grey-darker">Three</li>
+      </ul>
+
+  </div>
+  <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+            <div class="text-green-700 mb-2">Green-dark</div>
+      <ul class="list-reset leading-normal">
+          <li class="hover:text-green-700 text-grey-darker">One</li>
+          <li class="hover:text-green-700 text-grey-darker">Two</li>
+          <li class="hover:text-green-700 text-grey-darker">Three</li>
+      </ul>
+
+    <div class="text-green-300 mb-2 mt-4">Green-light</div>
+      <ul class="list-reset leading-normal">
+          <li class="hover:text-green-700 text-grey-darker">One</li>
+          <li class="hover:text-green-700 text-grey-darker">Two</li>
+          <li class="hover:text-green-700 text-grey-darker">Three</li>
+      </ul>
+  </div>
+    <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+        <div class="text-red-light mb-2">Newsletter</div>
+        <p class="text-grey-darker leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consectetur. </p>
+        <div class="mt-4 flex">
+            <input type="text" class="p-2 border border-grey-light round text-grey-dark text-sm h-auto" placeholder="Your email address"/>
+            <button class="bg-orange text-white rounded-sm h-auto text-xs p-3">Subscribe</button>
         </div>
-      </footer>
+    </div>
+
+</div>
+</div>
     )
 }
 export default Footer
