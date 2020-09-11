9890e021151547f527b04f923497075d1cd6d615
Shrey Jain <jainshreyp@gmail.com>
2020-09-11 10:40:02 -0400

    hello

--- src/src/pages/contact.js
+++ src/src/pages/contact.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import NavLink from 'react-router-dom'
+import emailjs from 'emailjs-com'
+
 
 function Contact() {
+    function sendMail() {
+    }
     return(
             <div className="">
                 <div className="mx-auto shadow-ld rounded-b-lg sm:mx-auto md:mx-auto max-w-full sm:max-w-full md:max-w-5xl relative">
@@ -48,6 +52,7 @@ function Contact() {
                             <button
                                 type="submit"
                                 className=" group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white shadow-xl bg-gray-900 hover:bg-orange-400 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                                onClick={sendMail()}
                                 >
                                 Submit
                             </button>
