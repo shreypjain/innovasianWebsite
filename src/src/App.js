7c07d100894b3f01f2903e2483d4f40e57489960
Shrey Jain <jainshreyp@gmail.com>
2020-09-23 10:13:58 -0400

    small update

--- src/src/App.js
+++ src/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/home"
 import Contact from "./pages/contact"
 import About from "./pages/about"
 import Sponsor from "./pages/sponsor"
+import Ticket from "./pages/ticket"
 
 import Footer from "./components/footer"
 import Navbar from "./components/navbar"
@@ -19,6 +20,7 @@ function App() {
             <Route path="/contact" component={Contact}/>
             <Route path="/about" component={About}/>
             <Route path="/sponsor" component={Sponsor}/>
+            <Route path="/ticket" component={Ticket}/>
             <Route path="/"component={Home}/>
           </Switch>
         <Footer/>
---
+++ src/src/pages/ticket.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Stripe from 'stripe'
+
+function Ticket() {
+    return(
+        <div>
+            Hello World
+        </div>
+    )
+}
+export default Ticket
